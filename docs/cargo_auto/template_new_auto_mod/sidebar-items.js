b3bc64a91907a744fd45965111252920dfd22109
luciano_bestia <info@bestia.dev>
2024-02-22 21:28:10 +0000

    update docs

--- docs/cargo_auto/template_new_auto_mod/sidebar-items.js
+++ docs/cargo_auto/template_new_auto_mod/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["build_automation_tasks_rs_if_needed","build_project_automation_tasks_rs","copy_to_files","get_vec_file","new_auto"]};
+window.SIDEBAR_ITEMS = {"fn":["compile_automation_tasks_rs_if_needed","compile_project_automation_tasks_rs","copy_to_files","get_vec_file","new_auto"]};
