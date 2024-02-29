a6cda6055aaddf03323a7c8fda3102589bd159bb
luciano_bestia <info@bestia.dev>
2024-02-29 02:12:36 +0000

    update docs

--- docs/src-files.js
+++ docs/src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = new Map(JSON.parse('[\
-["cargo_auto",["",[],["file_hashes_mod.rs","inside_of_rust_project_mod.rs","main.rs","outside_of_rust_project_mod.rs","template_new_auto_mod.rs","template_new_cli_mod.rs","template_new_pwa_wasm_mod.rs","template_new_wasm_mod.rs"]]]\
+["cargo_auto",["",[],["file_hashes_mod.rs","inside_of_rust_project_mod.rs","main.rs","outside_of_rust_project_mod.rs","template_new_auto_mod.rs","template_new_cli_mod.rs","template_new_pwa_wasm_mod.rs","template_new_wasm_mod.rs","utils_mod.rs"]]]\
 ]'));
 createSrcSidebar();
