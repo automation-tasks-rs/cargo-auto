ef418d861aa3a833b914eabc57284c85b6dc712c
bestia.dev <info@bestia.dev>
2024-04-01 00:03:12 +0000

    Revert "v"
    This reverts commit 54830c064c5e5a0479f5426bd78921829558c7b1.

--- template_new_pwa_wasm/web_server_folder/pwa_short_name/service_worker.js
+++ template_new_pwa_wasm/web_server_folder/pwa_short_name/service_worker.js
@@ -7,7 +7,7 @@
 // but the new service worker will not be activated until all 
 // tabs with this webapp are closed.
 
-const CACHE_NAME = '2024.331.2331';
+const CACHE_NAME = '2024.331.2226';
 
 self.addEventListener('install', event => {
     console.log('event install ', CACHE_NAME);
